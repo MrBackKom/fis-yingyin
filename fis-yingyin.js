c3c4a14d86b697cf7d83dce0a2c1f16214c72176
赵龙 <zhaolong324@163.com>
2014-09-19 00:21:24 +0800

    add new deps

--- fis-yingyin.js
+++ fis-yingyin.js
@@ -4,6 +4,34 @@ fis.cli.name = 'fis-yingyin';
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
 fis.config.merge({
+    roadmap : {
+        path : [
+            {
+                //一级通用组件组件，可以引用短路径，比如common/searchbox/searchbox.js
+                //直接引用为var searchbox = require('searchbox');
+                reg : /^\/common\/([^\/]+)\/\1\.(js)$/i,
+                //是组件化的，会被jswrapper包装
+                isMod : true,
+                //id为文件夹名
+                id : '$1'
+            },
+            {
+                //一级业务模块，可以引用短路径，比如modules/carton/carton.js
+                //直接引用为var carton = require('carton');
+                reg : /^\/modules\/([^\/]+)\/\1\.(js)$/i,
+                //是组件化的，会被jswrapper包装
+                isMod : true,
+                //id为文件夹名
+                id : '$1'
+            },
+            {
+                //其他css文件
+                reg : "**.css",
+                //css文件会做csssprite处理
+                useSprite : true
+            }
+        ]
+    },
     modules: {
         parser: {
             css: 'less',
@@ -11,8 +39,7 @@ fis.config.merge({
         },
         postprocessor: {
             js: 'jswrapper,require-async',
-            html: "require-async",
-            tpl: "require-async",
+            html: "require-async"
         },
 
         postpackager: ['autoload',  opermanifest],
@@ -27,17 +54,21 @@ fis.config.merge({
                 type: 'amd'
             }
         },
-        postpackager: {
-            autoload: {
-                useInlineMap: true,
-                include: '/page/**',
-                optDeps: false
+        postpackager:{
+            autoload:{
+                useSiteMap:true,
+                useInlineMap:true,
+                scriptTag:'<!--SCRIPT_PLACEHOLDER-->',
+                styleTag: '<!--STYLE_PLACEHOLDER-->',
+                resourceMapTag:'<!--RESOURCEMAP_PLACEHOLDER-->'
             }
         }
+
     }
 });
 
 function opermanifest(ret, conf, settings, opt){
+    console.log("opermanifest function called")
 }
 
 
